96d085f38a7e4d9d508d8f9ee78cb78c0031a758
Stephen <stephensellers034@Stephens-MacBook-Air.local>
2024-04-29 09:58:56 -0400

    My Javascript work

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Stephen";;
+let lastName;
+var age ;
 // Exercise 2
-
+lastName = "Sellers";
+age = 29;
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name Of The Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
